94105257a55be3f36ce462f4227df80a68dddbac
Ali Uneri <ali.uneri@jhu.edu>
2022-07-20 18:38:59 -0400

    Rename argument for grid shape

--- matsubplots.py
+++ matsubplots.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import Grid, ImageGrid
 
 
-def axesgrid(grid=1, size=3, pad=0, close=False, image_grid=False, return_grid=False, **kwargs):
-    if np.isscalar(grid):
-        grid = grid, 1
+def axesgrid(shape=1, size=3, pad=0, close=False, image_grid=False, return_grid=False, **kwargs):
+    if np.isscalar(shape):
+        shape = shape, 1
     if np.isscalar(size):
         size = np.repeat(size, 2)
     if np.isscalar(pad):
@@ -13,13 +13,13 @@ def axesgrid(grid=1, size=3, pad=0, close=False, image_grid=False, return_grid=F
     xticks = kwargs.pop('xticks', None)
     yticks = kwargs.pop('yticks', None)
     frameon = kwargs.pop('frameon', None)
-    figsize = [size[x] * grid[x] + pad[x] * (grid[x] + 1) for x in range(2)]
+    figsize = [size[x] * shape[x] + pad[x] * (shape[x] + 1) for x in range(2)]
     fig = plt.figure(figsize=figsize)
     if close:
         plt.close(fig)
     padr = [pad[x] / figsize[x] for x in range(2)]
     rect = padr[0], padr[1], 1 - padr[0] * 2, 1 - padr[1] * 2
-    axs = (ImageGrid if image_grid else Grid)(fig, rect, grid[::-1], axes_pad=pad, **kwargs)
+    axs = (ImageGrid if image_grid else Grid)(fig, rect, shape[::-1], axes_pad=pad, **kwargs)
     for ax in axs:
         if xticks is not None:
             ax.set_xticks(xticks)
@@ -38,20 +38,20 @@ def imagegrid(*args, **kwargs):
     return axesgrid(*args, **kwargs)
 
 
-def subplots(grid=1, size=3, pad=0, close=False, label_mode='L', **kwargs):
-    if np.isscalar(grid):
-        grid = grid, 1
+def subplots(shape=1, size=3, pad=0, close=False, label_mode='L', **kwargs):
+    if np.isscalar(shape):
+        shape = shape, 1
     if np.isscalar(size):
         size = np.repeat(size, 2)
     if np.isscalar(pad):
         pad = np.repeat(pad, 2)
-    figsize = [size[x] * grid[x] + pad[x] * (grid[x] + 1) for x in range(2)]
+    figsize = [size[x] * shape[x] + pad[x] * (shape[x] + 1) for x in range(2)]
     fig = plt.figure(figsize=figsize)
     if close:
         plt.close(fig)
-    axs = np.empty(grid[::-1], dtype=np.object)
-    for i in range(grid[0]):
-        for j in range(grid[1]):
+    axs = np.empty(shape[::-1], dtype=np.object)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
             offset = [pad[x] + (i,j)[x] * (size[x] + pad[x]) for x in range(2)]
             axs[j,i] = _add_axes_inches(fig, size, offset, origin='top left', **kwargs)
     if label_mode == 'L':
