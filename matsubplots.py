0a197ef1edf51260f5b8b1ed2fa3d6e65181b9d0
Ali Uneri <ali.uneri@jhu.edu>
2022-07-20 18:40:08 -0400

    Better comments

--- matsubplots.py
+++ matsubplots.py
@@ -4,6 +4,7 @@ from mpl_toolkits.axes_grid1 import Grid, ImageGrid
 
 
 def axesgrid(shape=1, size=3, pad=0, close=False, image_grid=False, return_grid=False, **kwargs):
+    """Extend mpl_toolkits.axes_grid1.Grid."""
     if np.isscalar(shape):
         shape = shape, 1
     if np.isscalar(size):
@@ -33,12 +34,14 @@ def axesgrid(shape=1, size=3, pad=0, close=False, image_grid=False, return_grid=
 
 
 def imagegrid(*args, **kwargs):
+    """Extend mpl_toolkits.axes_grid1.ImageGrid."""
     kwargs['image_grid'] = True
     kwargs.setdefault('aspect', False)
     return axesgrid(*args, **kwargs)
 
 
 def subplots(shape=1, size=3, pad=0, close=False, label_mode='L', **kwargs):
+    """Extend matplotlib.pyplot.subplots."""
     if np.isscalar(shape):
         shape = shape, 1
     if np.isscalar(size):
