666b9848fe6a80f87826c229fb15a77e311409b3
Ali Uneri <auneri@users.noreply.github.com>
2024-04-25 18:26:17 -0400

    Rename module to allow reloading

--- matsubplots/__init__.py
+++ matsubplots/__init__.py
@@ -1,3 +1,3 @@
 from .version import __version__  # noqa: F401
 from .matsubplots import grid, imagegrid, subplots  # noqa: F401, I100
-from .orthoview import orthoview  # noqa: F401
+from .viewers import orthoview  # noqa: F401
