0edb5b541a2a735a5b5f74b0d7b523e8bf5702fb
Ali Uneri <auneri@users.noreply.github.com>
2024-05-07 17:17:14 -0400

    Reimplement orthoview as a class

--- matsubplots/viewers.py
+++ matsubplots/viewers.py
@@ -2,34 +2,77 @@ import numpy as np
 from matplotlib.backend_bases import MouseButton
 
 
-def orthoview(axs, image, spacing=(1,1,1), xyz=None, ijk=None, slab_thickness=None, slab_func=np.mean, reposition=True, backend=None, **kwargs):
-    axs = np.asarray(axs)
-    image = np.asarray(image)
-    spacing = np.asarray(spacing)
-    if not (axs.size == image.ndim == spacing.size == 3):
-        raise ValueError('Expected a 3D image, 3 axes, and 3 values for spacing')
-    if ijk is not None and xyz is not None:
-        raise ValueError('Cannot specify ijk and xyz at the same time')
-    bounds = []
-    for i, ax in enumerate(axs.ravel()):
-        if xyz is not None:
-            j = round(xyz[::-1][i] / spacing[::-1][i] + (image.shape[i] - 1) / 2)
-        elif ijk is not None:
-            j = ijk[i]
-        else:
+def orthoview(*args, backend=None, **kwargs):
+    if backend is None:
+        return OrthoView(*args, **kwargs)
+    elif backend.lower() == 'interactive':
+        return OrthoViewInteractive(*args, **kwargs)
+    elif backend.lower() == 'static':
+        return OrthoViewStatic(*args, **kwargs)
+
+
+class OrthoView:
+
+    alignments = (0, 0), (0, 1), (1, 1)
+    colors = '#ff0000', '#00ff00', '#ffff00'
+    indices = (1, 2), (0, 2), (0, 1)
+
+    def __init__(self, axs, image, spacing=(1,1,1), slab_thickness=None, slab_func=np.mean, reposition=True, **kwargs):
+        axs = np.asarray(axs)
+        image = np.asarray(image)
+        spacing = np.asarray(spacing)
+        if not (axs.size == image.ndim == spacing.size == 3):
+            raise ValueError('Expected a 3D image, 3 axes, and 3 values for spacing')
+        bounds = []
+        for i, ax in enumerate(axs.ravel()):
             j = image.shape[i] // 2
-        thickness = 1 if slab_thickness is None else round(slab_thickness / spacing[i])
-        j0 = np.maximum(j - thickness // 2, 0)
-        j1 = j - (-thickness // 2)
-        slice_ = slab_func(np.rollaxis(image, i)[j0:j1], axis=0)
-        aspect = np.divide(*spacing[::-1][np.arange(spacing.size) != i])
-        bounds.append([])
-        bounds[-1].append(ax.get_position().bounds)
-        im = ax.imshow(slice_, aspect=aspect, **kwargs)
-        bounds[-1].append(ax.get_position().bounds)
-        if hasattr(ax, 'cax'):
-            ax.get_figure().colorbar(im, cax=ax.cax)
-    if reposition:
+            thickness = 1 if slab_thickness is None else round(slab_thickness / spacing[i])
+            j0 = np.maximum(j - thickness // 2, 0)
+            j1 = j - (-thickness // 2)
+            slice_ = slab_func(np.rollaxis(image, i)[j0:j1], axis=0)
+            aspect = np.divide(*spacing[::-1][np.arange(spacing.size) != i])
+            bounds.append([])
+            bounds[-1].append(ax.get_position().bounds)
+            im = ax.imshow(slice_, aspect=aspect, **kwargs)
+            bounds[-1].append(ax.get_position().bounds)
+            if hasattr(ax, 'cax'):
+                ax.get_figure().colorbar(im, cax=ax.cax)
+        crosshairs = [[
+            axs[i].axhline(image.shape[x]//2, lw=1, c=self.colors[x], visible=False),
+            axs[i].axvline(image.shape[y]//2, lw=1, c=self.colors[y], visible=False)]
+            for i, (x,y) in enumerate(self.indices)]
+        self.axs = axs
+        self.image = image
+        self.spacing = spacing
+        self._crosshairs = crosshairs
+        if reposition:
+            self._reposition(axs, bounds)
+
+    def crosshairs(self, toggle=None):
+        if toggle is None:
+            toggle = not self._crosshairs[0][0].get_visible()
+        for i, crosshairs in enumerate(self._crosshairs):
+            for line in crosshairs:
+                line.set_visible(toggle)
+
+    def ijk(self, i, j, k, crosshairs=None):
+        for index, value in enumerate((i, j, k)):
+            self._scroll(index, value)
+        if crosshairs is not None:
+            self.crosshairs(crosshairs)
+
+    def xyz(self, x, y, z, crosshairs=None):
+        xyz = x, y, z
+        ijk = [round(xyz[::-1][i] / self.spacing[::-1][i] + (self.image.shape[i] - 1) / 2) for i in range(3)]
+        self.ijk(*ijk, crosshairs=crosshairs)
+
+    def _scroll(self, index, value):
+        self.axs[index].images[0].set_data(np.rollaxis(self.image, index)[value])
+        for i, alignment in zip(self.indices[index], self.alignments[index]):
+            getattr(self._crosshairs[i][alignment], 'set_ydata' if alignment == 0 else 'set_xdata')([value, value])
+
+    @staticmethod
+    def _reposition(axs, bounds):
         _, _, w00, _ = bounds[0][0]
         l01, b01, w01, h01 = bounds[0][1]
         l0, b0, w0, h0 = l01, b01, w01, h01
@@ -67,41 +110,6 @@ def orthoview(axs, image, spacing=(1,1,1), xyz=None, ijk=None, slab_thickness=No
             ch2 = max(h0, h1, h2)
             cb2 = cb + (ch - ch2) / 2
             axs[-1].cax.set_position((cl - lshift, cb2, cw, ch2))
-    if backend is None:
-        pass
-    elif backend.lower() == 'interactive':
-        return OrthoViewInteractive(axs, image)
-    elif backend.lower() == 'static':
-        return OrthoViewStatic(axs, image)
-    else:
-        raise NotImplementedError(backend)
-
-
-class OrthoView:
-
-    alignments = (0, 0), (0, 1), (1, 1)
-    colors = '#ff0000', '#00ff00', '#ffff00'
-    indices = (1, 2), (0, 2), (0, 1)
-
-    def __init__(self, axs, image):
-        self.axs = axs
-        self.image = image
-        self.crosshairs = [[
-            axs[i].axhline(image.shape[x]//2, lw=1, c=self.colors[x]),
-            axs[i].axvline(image.shape[y]//2, lw=1, c=self.colors[y])]
-            for i, (x,y) in enumerate(self.indices)]
-        for x in self.crosshairs:
-            for y in x:
-                y.set_visible(False)
-
-    def scroll(self, i, j, k):
-        for index, value in enumerate((i, j, k)):
-            self.scrolli(index, value)
-
-    def scrolli(self, index, value):
-        self.axs[index].images[0].set_data(np.rollaxis(self.image, index)[value])
-        for i, alignment in zip(self.indices[index], self.alignments[index]):
-            getattr(self.crosshairs[i][alignment], 'set_ydata' if alignment == 0 else 'set_xdata')([value, value])
 
 
 class OrthoViewInteractive(OrthoView):
@@ -126,9 +134,7 @@ class OrthoViewInteractive(OrthoView):
     def on_press(self, event):
         if event.inaxes in self.axs:
             if event.dblclick:
-                for x in self.crosshairs:
-                    for y in x:
-                        y.set_visible(not y.get_visible())
+                self.crosshairs()
             elif event.button == MouseButton.LEFT:
                 self.pressed[0] = event
             elif event.button == MouseButton.RIGHT:
@@ -142,7 +148,7 @@ class OrthoViewInteractive(OrthoView):
         for i, ax in enumerate(self.axs):
             if self.pressed[0] is not None and event.inaxes is ax:
                 for j, index in enumerate(self.indices[i]):
-                    self.scrolli(index, round(getattr(event, 'ydata' if j == 0 else 'xdata')))
+                    self._scroll(index, round(getattr(event, 'ydata' if j == 0 else 'xdata')))
             elif self.pressed[1] is not None and event.inaxes is ax:
                 vmin, vmax = self.axs[0].images[0].get_clim()
                 win = vmax - vmin
@@ -168,7 +174,7 @@ class OrthoViewStatic(OrthoView):
         super().__init__(axs, image, *args, **kwargs)
         self.wslices = [ipywidgets.IntSlider(description=x, value=image.shape[i]//2, min=0, max=image.shape[i]-1) for i, x in enumerate('ijk')]
         self.wclim = ipywidgets.FloatRangeSlider(description='clim', value=axs[0].images[0].get_clim(), min=np.min(image), max=np.max(image))
-        self.wcrosshairs = ipywidgets.Checkbox(description='crosshairs', value=self.crosshairs[0][0].get_visible())
+        self.wcrosshairs = ipywidgets.Checkbox(description='crosshairs', value=self._crosshairs[0][0].get_visible())
         self.woutput = ipywidgets.Output()
         with self.woutput:
             display(axs[0].get_figure())
@@ -179,11 +185,9 @@ class OrthoViewStatic(OrthoView):
                 for ax in axs:
                     ax.images[0].set_clim(change['new'])
             elif change['owner'] is self.wcrosshairs:
-                for x in self.crosshairs:
-                    for y in x:
-                        y.set_visible(change['new'])
+                self.crosshairs(change['new'])
             elif change['owner'] in self.wslices:
-                self.scrolli(self.wslices.index(change['owner']), change['new'])
+                self._scroll(self.wslices.index(change['owner']), change['new'])
             display(axs[0].get_figure())
 
         for widget in self.wslices + [self.wclim, self.wcrosshairs]:
